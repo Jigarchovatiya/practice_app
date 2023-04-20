b81f2aefa634c834790c309e4323c8d8b3bbbb8c
jigar chovatiya <115682188+Jigarchovatiya@users.noreply.github.com>
2023-04-20 15:58:17 +0530

    20-04-2023

--- lib/picker/video_picker.dart
+++ lib/picker/video_picker.dart
@@ -2,113 +2,59 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoSelector extends StatefulWidget {
-  const VideoSelector({super.key});
+  const VideoSelector({Key? key}) : super(key: key);
 
   @override
   State<VideoSelector> createState() => _VideoSelectorState();
 }
 
 class _VideoSelectorState extends State<VideoSelector> {
-  File? galleryFile;
-
+  File? _video;
+  VideoPlayerController? _videoPlayerController;
   final picker = ImagePicker();
-  @override
-  Widget build(BuildContext context) {
-    // display image selected from gallery
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Gallery and Camera Access'),
-        backgroundColor: Colors.green,
-        actions: const [],
-      ),
-      body: Builder(
-        builder: (BuildContext context) {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.green)),
-                  child: const Text('Select video from Gallery and Camera'),
-                  onPressed: () {
-                    _showPicker(context: context);
-                  },
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  height: 200,
-                  width: 300,
-                  child: galleryFile == null
-                      ? const Center(
-                          child: Text('Sorry nothing selected!!'),
-                        )
-                      : Center(child: Text(galleryFile!.path)),
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 18),
-                  child: Text(
-                    "GFG",
-                    textScaleFactor: 3,
-                    style: TextStyle(color: Colors.green),
-                  ),
-                )
-              ],
-            ),
-          );
-        },
-      ),
-    );
+  _pickVideo() async {
+    final video = await picker.pickVideo(source: ImageSource.gallery);
+    _video = File(video!.path);
+    _videoPlayerController = VideoPlayerController.file(_video!)
+      ..initialize().then((value) {
+        setState(() {});
+
+        _videoPlayerController!.pause();
+      });
   }
 
-  void _showPicker({
-    required BuildContext context,
-  }) {
-    showModalBottomSheet(
-      context: context,
-      builder: (BuildContext context) {
-        return SafeArea(
-          child: Wrap(
-            children: <Widget>[
-              ListTile(
-                leading: const Icon(Icons.photo_library),
-                title: const Text('Gallery'),
-                onTap: () {
-                  getVideo(ImageSource.gallery);
-                  Navigator.of(context).pop();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Video Player"),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              if (_video != null)
+                _videoPlayerController!.value.isInitialized
+                    ? AspectRatio(
+                        aspectRatio: _videoPlayerController!.value.aspectRatio,
+                        child: VideoPlayer(_videoPlayerController!),
+                      )
+                    : Container()
+              else
+                const Text("click on pick video to select video"),
+              ElevatedButton(
+                onPressed: () {
+                  _pickVideo();
                 },
-              ),
-              ListTile(
-                leading: const Icon(Icons.photo_camera),
-                title: const Text('Camera'),
-                onTap: () {
-                  getVideo(ImageSource.camera);
-                  Navigator.of(context).pop();
-                },
+                child: const Text("pick video file from gallery"),
               ),
             ],
           ),
-        );
-      },
-    );
-  }
-
-  Future getVideo(
-    ImageSource img,
-  ) async {
-    final pickedFile = await picker.pickVideo(source: img, preferredCameraDevice: CameraDevice.front, maxDuration: const Duration(minutes: 10));
-    XFile? xfilePick = pickedFile;
-    setState(
-      () {
-        if (xfilePick != null) {
-          galleryFile = File(pickedFile!.path);
-        } else {
-          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Nothing is selected')));
-        }
-      },
+        ),
+      ),
     );
   }
 }
